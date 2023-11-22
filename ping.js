935ee8c9ba5116606475699fedb6daab850b10ea
Marcus Vinicius de Paula Silveira <e.marcus.silveira@brde.com.b>
2023-11-22 15:33:37 -0300

    corrigido import

--- ping.js
+++ ping.js
@@ -1,18 +1,26 @@
-const fetch = require('node-fetch').default;
+const https = require('https');
 
 // Função para fazer a requisição GET à URL
-const pingURL = async () => {
-  try {
-    const response = await fetch('https://opportunity-api-ybe0.onrender.com/api');
-    
-    if (response.ok) {
+const pingURL = () => {
+  const options = {
+    method: 'GET',
+    hostname: 'opportunity-api-ybe0.onrender.com',
+    path: '/api'
+  };
+
+  const req = https.request(options, (res) => {
+    if (res.statusCode === 200) {
       console.log('Ping bem-sucedido em', new Date().toLocaleString());
     } else {
-      console.error('Erro no ping:', response.statusText);
+      console.error('Erro no ping:', res.statusCode, res.statusMessage);
     }
-  } catch (error) {
+  });
+
+  req.on('error', (error) => {
     console.error('Erro ao fazer ping:', error.message);
-  }
+  });
+
+  req.end();
 };
 
 // Realiza o ping a cada 5 minutos (300.000 milissegundos)
